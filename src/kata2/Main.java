6a80dbd2facaabc7b95d0ee1b798cefca0cd6812
Maria <bajena87@gmail.com>
2019-10-15 17:44:02 +0200

    KATA2 Version 1 revision para entrenar

--- src/kata2/Main.java
+++ src/kata2/Main.java
@@ -3,17 +3,23 @@ package kata2;
 import java.util.HashMap;
 import java.util.Map;
 
+
+
 public class Main {
 
     public static void main(String[] args) {
-    String[] data = {"Rosa", "Pepe", "María", "Pepe", "Pepe", "Rosa"};
-    
-    Histogram histo = new Histogram(data);
-    Map<String,Integer> histogr = histo.getHistogram();
-    
-        for (String key : histogr.keySet()) {
-            System.out.println(key + "==>" + histogr.get(key));
-    
+        int[] data = {1,2,3,4,5,6,7,8,9,1,2,3};
+        
+        HashMap<Integer, Integer> histogram = new HashMap();
+        for (int i = 0; i < data.length; i++) {
+            int j = data[i];
+            if (histogram.containsKey(j))
+                histogram.put(j, histogram.get(j)+1);
+            else histogram.put(j, 1);
         }
+        
+        for (Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
+            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+        } 
     }
 }
